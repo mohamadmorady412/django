7c7b35d775ff4b17cac5a6c8a77ac06cb5042119
mohamadmorady412 <mohamadmorady412@gmail.com>
2025-03-16 11:14:11 +0330

    docker compose
    + dc
    + envs in django setting

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5g%!ycylf*gg6qd9wyby7!@6godkx7vgd%_i@d&cg0vl3sivk)'
+SECRET_KEY = config("SECRET_KEY")#'django-insecure-5g%!ycylf*gg6qd9wyby7!@6godkx7vgd%_i@d&cg0vl3sivk)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)#True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS_str = config("ALLOWED_HOSTS", default="")
+ALLOWED_HOSTS = [s.strip() for s in ALLOWED_HOSTS_str.split(',')] if ALLOWED_HOSTS_str else []
 
 
 # Application definition
